aebe9447baf0491f4e592bd16a9e0f37b555819e
Elham <elitohidi42@gmail.com>
2026-07-17 11:16:36 +0330

    update

--- Data_an.py
+++ Data_an.py
@@ -1,31 +1,35 @@
 import pandas as pd
 
 
-## Reading csv file
-
+### Reading csv file
 Raw_Data = pd.read_csv("Raw_data.csv")
 Raw_Data = pd.DataFrame(Raw_Data , columns = ["Time","Feedingscrew_F","Feedingscrew_Am","Rotarywing_F",
                                                   "Rotarywing_Am","Idfan_F","Idfan_Am","Temp_Inlet","Temp_Outlet"
-                                                  ,"Temp_Exhaust","Feeding_rate","A","B"])
+                                                  ,"Temp_Exhaust","Feeding_rate","Stage","Status"])
 
-## Data cleaning
+### Data Prepration
 
-# Data shape
-print (Raw_Data.shape)
+# Data inspection in first stage
+print (f" the data shape is:{Raw_Data.shape}")
+print (f" data type in columns is: {Raw_Data.dtypes}")
+print (f"")
 
-# Removing exctra columns 
-Raw_Data = Raw_Data.drop(columns = ["A" , "B"])
-
-#  Nulls
-#print (Raw_Data.isnull())
-Nal_count = Raw_Data.isnull().sum()
-totalNal_count = Raw_Data.isnull().sum().sum()
-print (Nal_count,totalNal_count)
+#  Nulls Report & Desicioning
+null_report = pd.DataFrame({
+    "Null Count": Raw_Data.isnull().sum(),
+    "Null Percent": (Raw_Data.isnull().mean() * 100).round(2)
+})
+#print(null_report)
+#print (f"based on the {null_report} we need to remove Stage & Status columns")
+# Removing exctra columns that are nulls
+Raw_Data = Raw_Data.drop(columns = ["Stage" , "Status"])
+# checking Uniqe values
+#print (Raw_Data["Feeding_rate"].unique())
 #Raw_Data = Raw_Data.dropna()
 
 # Removing duplicated rows
 #print(Raw_Data.duplicated())
-Raw_Data.drop_duplicates(inplace = True)
+#Raw_Data.drop_duplicates(inplace = True)
 
 #print (Raw_Data.shape)
 
